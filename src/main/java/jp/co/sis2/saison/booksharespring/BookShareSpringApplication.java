f4bb37425b0c8102bbc46cac165b56858da7284d
YuyaTorii <Yuya_Torii@hulft.com>
2022-11-08 00:16:40 +0900

    first commit

---
+++ src/main/java/jp/co/sis2/saison/booksharespring/BookShareSpringApplication.java
@@ -0,0 +1,13 @@
+package jp.co.sis2.saison.booksharespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookShareSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookShareSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jp/co/sis2/saison/booksharespring/BookShareSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.co.sis2.saison.booksharespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookShareSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
